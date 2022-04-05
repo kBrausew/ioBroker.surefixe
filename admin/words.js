f9cbbc7cdb998156a1e508157b1cb1f076326d8e
kbrausew <kurt.brausewetter@t-online.de>
2022-04-05 20:59:18 +0200

    first init

---
+++ admin/words.js
@@ -0,0 +1,53 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "surefixe adapter settings": {
+        "en": "Adapter settings for surefixe",
+        "de": "Adaptereinstellungen für surefixe",
+        "ru": "Настройки адаптера для surefixe",
+        "pt": "Configurações do adaptador para surefixe",
+        "nl": "Adapterinstellingen voor surefixe",
+        "fr": "Paramètres d'adaptateur pour surefixe",
+        "it": "Impostazioni dell'adattatore per surefixe",
+        "es": "Ajustes del adaptador para surefixe",
+        "pl": "Ustawienia adaptera dla surefixe",
+        "zh-cn": "surefixe的适配器设置"
+    },
+    "battery": {
+        "en": "Battery Report",
+        "de": "Batteriebericht",
+        "ru": "Отчет о батарее",
+        "pt": "Relatório de bateria",
+        "nl": "Batterijrapport",
+        "fr": "Rapport de batterie",
+        "it": "Rapporto sulla batteria",
+        "es": "Informe de batería",
+        "pl": "Raport dotyczący baterii",
+        "zh-cn": "电池报告"
+    },
+    "reachable": {
+        "en": "Un/Reachable Report",
+        "de": "Unerreichbar-Bericht",
+        "ru": "Недоступный отчет",
+        "pt": "Relatório inacessível",
+        "nl": "On/bereikbaar rapport",
+        "fr": "Rapport in/accessible",
+        "it": "Rapporto irraggiungibile",
+        "es": "Informe inaccesible/inaccesible",
+        "pl": "Raport nieosiągalny",
+        "zh-cn": "不可达/不可达报告"
+    },
+    "option3": {
+        "en": "Option",
+        "de": "Möglichkeit",
+        "ru": "Вариант",
+        "pt": "Opção",
+        "nl": "Optie",
+        "fr": "Option",
+        "it": "Opzione",
+        "es": "Opción",
+        "pl": "Opcja",
+        "zh-cn": "选项"
+    }
+};
